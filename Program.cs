576943b62af1fce7bcb3f53a1a146b067101875b
Brunopln12 <bruno_pinho_lobo@hotmail.com>
2023-12-24 16:38:26 -0300

    Todas as implementações feitas com sucesso.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("223145", "Modelo d20", "2345", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+Console.WriteLine($"Dados do Smatphone: Número: {nokia.Numero}");
+nokia.InstalarAplicativo("Deezer");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone("123456", "Nokia Modelo X", "123WE32", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+Console.WriteLine($"Dados do Smartphone: Número: {iphone.Numero}");
+iphone.InstalarAplicativo("Globo Play");
